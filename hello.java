0c3fb274dab16e7a4ddfb5b059bba7bc26b36d94
William Pabst <pabst.william@gmail.com>
2018-03-19 10:32:20 -0400

    Added functionality to receive user input, and ouput a personalized message.

--- hello.java
+++ hello.java
@@ -1,15 +1,16 @@
 // a simple program to practice common Github commands 
-
+import java.util.Scanner;
 public class hello {
 
     public static void main(String [] args) {
 
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter your name: ");
+        String s = reader.nextLine();
+        reader.close();
 
-	
-	// added  line	
-	// added another line
 
-        System.out.println("Hello Class!");
+        System.out.println("Hello " + s + ", my name is Billy!");
 
 
 
